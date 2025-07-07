b0b4ecdb430489dbc98e63b3c925d01a66b42a72
FikruDemlew <fikrudemlew@gmail.com>
2025-07-07 21:36:21 +0300

    documtation setup

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import Header from './components/Header.vue';
+import Balance from './components/Balance.vue';
+import IncomeExpense from './components/IncomeExpense.vue';
+import TransactionList from './components/TransactionList.vue';
+import AddTransaction from './components/AddTransaction.vue';
+export default {
+  components : {
+    Header,
+    Balance,
+    IncomeExpense,
+    TransactionList,
+    AddTransaction,
+  }
+}
+</script>
+ 
+
+
+<template>
+  <Header/>
+  <div class="container">
+    <Balance/>
+    <IncomeExpense/>
+    <TransactionList/>
+    <AddTransaction/>
+  </div>
+</template>
