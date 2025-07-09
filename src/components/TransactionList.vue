99407db8ced4d9a8d0930f278f65f9658fe69148
FikruDemlew <fikrudemlew@gmail.com>
2025-07-09 15:11:01 +0300

    add transaction list

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,16 +1,12 @@
-<script>
-export default {
-    data() {
-        return {
-            transactions: [
-                {id: 1, text: 'Flower', amount: -19.99 },
-                {id: 2, text: 'Salary', amount: 29.997 },
-                {id: 3, text: 'Book', amount: -10},
-                {id: 4, text: 'Camera', amount: 150 },
-            ]
-        }
-    }
-}
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    transactions: {
+        type: Array,
+        required: true,
+    },
+})
 </script>
 
 <template>
@@ -20,12 +16,6 @@ export default {
             {{ transaction.text }}<span>${{ transaction.amount }}</span><button class="delete-btn">x</button>
 
         </li>
-        <!-- <li class="minus">
-        Cash <span>-$400</span><button class="delete-btn">x</button>
-        </li>
-        <li class="plus">
-        Paycheck <span>$900</span><button class="delete-btn">x</button>
-        </li> -->
     </ul>
     
 </template>
